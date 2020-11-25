813f5c2ebb3dff53cf4e465a1b5bf85a45680e71
罗友 <luoyou@dml-express.com>
2020-11-25 22:12:21 +0800

    complate arrayList

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+
 using namespace std;
 
 #include "src/study/Study.hpp"
@@ -7,11 +8,12 @@ using namespace std;
 #include "src/object/HiObject.hpp"
 #include "src/object/HiString.hpp"
 #include "src/util/ArrayList.hpp"
+
 #define INIT 0
 #define NUM 1
 
 
-int main(int argc,char** argv) {
+int main(int argc, char **argv) {
 
     /***
      * Study space
@@ -28,10 +30,48 @@ int main(int argc,char** argv) {
     /***
      * Test space
      */
-     ArrayList<int > xx(1);
-     xx.add(3);
-     xx.add(4);
-     printf("%d\n",xx.pop());
+    ArrayList<int> xx(5);
+    xx.push(1);
+    xx.push(2);
+    xx.push(3);
+    xx.push(4);
+    xx.push(5);
+    xx.push(6);
+    xx.push(7);
+    xx.push(8);
+    xx.push(9);
+    xx.pop();
+    xx.pop();
+    xx.pop();
+    xx.pop();
+    xx.pop();
+    xx.pop();
+    xx.pop();
+
+    printf("%d\n", xx.size());
+    printf("%d\n", xx.capacity());
+    printf("%d\n\n\n", xx.length());
+
+    xx.insert(0, 0);
+    printf("size:%d\n", xx.size());
+    xx.remove(4);
+    printf("size:%d\n", xx.size());
+    xx.set(4, 4);
+    xx.set(6, 4);
+
+
+    printf("size:%d\n", xx.size());
+
+    printf("%d\n", xx.get(0));
+    printf("%d\n", xx.get(1));
+    printf("%d\n", xx.get(2));
+    printf("%d\n", xx.get(3));
+    printf("%d\n", xx.get(4));
+    printf("%d\n", xx.get(5));
+    printf("%d\n", xx.get(6));
+    printf("%d\n", xx.size());
+    printf("%d\n", xx.capacity());
+    printf("%d\n", xx.length());
 
 
     /***
--- src/util/ArrayList.hpp
+++ src/util/ArrayList.hpp
@@ -4,18 +4,16 @@
 
 #ifndef PYTHONVM_ARRAYLIST_HPP
 #define PYTHONVM_ARRAYLIST_HPP
-#define PRINT_CLASS_INFO_BEGIN(sstream, className) sstream << "ClassName:" << #className << ", Addr:" << this << "[";
-#define PRINT_CLASS_PARA(sstream, para) sstream << #para << " = " << para;
-#define PRINT_CLASS_INFO_END(sstream) sstream << "]" << std::endl;
+
+#include <assert.h>
 
 template<typename T>
 class ArrayList {
 private:
 private:
-    int _length;//元素数组大小
+    int _capacity;//元素数组大小
     T *_array;//泛型指针,指向元素数组
     int _size;//用于显示当前列表中的有效元素个数
-
     /**
      * 在hpp中可以将模板类的实现和定义放在一起
      * 分开实现：
@@ -23,97 +21,116 @@ private:
      * 实现：template<typename T> void ArrayList<T>::expand(){}
      * 一起实现：如下
      */
-    void expand();
+    void resize(int capacity) {
+        assert(capacity >= _size);
+        T *new_array = new T[capacity];
+        for (int i = 0; i < _size; i++)
+            new_array[i] = _array[i];
+        delete[] _array;
+        _array = new_array;
+        _capacity = capacity;
+    }
 
 public:
-    ArrayList(int n = 8);
-    void add(T t);
+    ArrayList(int capacity = 8) {
+        _capacity = capacity;
+        _size = 0;
+        _array = new T[_capacity];
+    }
 
-    void insert(int index, T t);
+    ~ArrayList() {
+        delete[] _array;
+    }
 
-    T get(int index);
+    void push(T t) {
+        /**
+         * 将元素放到arrayList的末尾
+         */
+        assert(_size <= _capacity);
+        if (_size == _capacity) {
+            resize(_capacity << 1);
+        }
+        _array[_size++] = t;
+    }
 
-    void set(int index, T t);
+    T pop() {
+        /**
+         * 移除arrayList的最后一个元素
+         */
+        assert(_size > 0);
+        T back = _array[_size--];
+        if (_size == _capacity >> 2) {
+            resize(_capacity >> 1);
+        }
+        return back;
+    }
+
+    void insert(int index, T t) {
+        /**
+         * 向指定的位置插入元素
+         */
+        push(NULL);//添加一个元素到末尾
+        for (int i = _size; i > index; i--) {
+            _array[i] = _array[i - 1];
+        }
+        _array[index] = t;
+    }
+
+    void remove(int index){
+        /**
+         * 移除指定位置的元素
+         */
+         if (index<_size){
+             for (int i = index; i < _size; i++) {
+                 _array[i] = _array[i+1];
+             }
+             pop();
+         }
+    }
+
+    T get(int index) {
+        /**
+         * 获取指定索引的数据
+         */
+        assert(index<_size);
+        return _array[index];
+    }
+
+    void set(int index, T t) {
+        /**
+         * 设置指定索引的数据
+         */
+        if (_size <= index) {
+            _size = index + 1;
+        }
+        if (_size > _capacity) {
+            resize(index<<1);
+        }
+        _array[index] = t;
+    }
 
     int size() {
+        /**
+         * 返回有效元素数量
+         */
         return _size;
     }
 
-    int length() {
-        return _length;
+    int capacity() {
+        /**
+         * 元素数组的容量
+         */
+        return _capacity;
     }
 
-    T pop();
+    int length() {
+        /**
+         * 元素数组的长度
+         */
+        return _capacity;
+    }
 
 };
-
-template<typename T>
-void ArrayList<T>::expand() {
-    /**
-     * 当有效元素数量超过元素数组的容量时，可以扩展数组
-     */
-    T *new_array = new T[_length << 1];//_length*2
-    for (int i = 0; i < _length; ++i) {
-        new_array[i] = _array[i];
-    }
-    _array = new_array;
-    delete[] _array;
-    _length <<= 1;
-    printf("expand an array to %d,size is %d\n", _length, _size);
-}
-
-template<typename T>
-ArrayList<T>::ArrayList(int n) {
-    _length = n;
-    _size = 0;
-    _array = new T[n];
-}
-
-template<typename T>
-void ArrayList<T>::add(T t) {
-    /**
-     * 向列表的末尾添加元素
-     */
-    if (_size >= _length) {
-        expand();
-    }
-    _array[_size++] = t;
-}
-
-template<typename T>
-void ArrayList<T>::insert(int index, T t) {
-    /**
-     * 向指定的位置插入元素
-     */
-    add(NULL);//添加到末尾
-    for (int i = _size; i > index; i--) {
-        _array[i] = _array[i - 1];
-    }
-    _array[index] = t;
-
-}
-
-template<typename T>
-T ArrayList<T>::get(int index) {
-    return _array[index];
-}
-
-template<typename T>
-void ArrayList<T>::set(int index, T t) {
-    if (_size <= index) {
-        _size = index + 1;
-    }
-    while (_size > _length) {
-        expand();
-    }
-    _array[index] = t;
-}
-
-template<typename T>
-T ArrayList<T>::pop() {
-    return _array[--_size];
-}
-
 
 
 #endif //PYTHONVM_ARRAYLIST_HPP
