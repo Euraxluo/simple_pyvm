56fcadb98d8ca1b49fc49104351aa43256b21ba7
Euraxluo <1793939176@qq.com>
2021-03-16 00:25:21 +0800

    obj init doen

--- src/runtime/universe.cpp
+++ src/runtime/universe.cpp
@@ -27,10 +27,28 @@ void Universe::genesis(){
     Klass* object_klass = ObjectKlass::getInstance();
     Klass* type_klass   = TypeKlass::getInstance();
 //    Klass* int_klass    = IntegerKlass::getInstance();
+
+
+    //init type_klass
     Type* type = new Type();//初始化type
-    type_klass->setName(new String("type"));//新建一个类型
     type->setSign(type_klass); //为type_obj设置类型
     type_klass->setSuper(object_klass); //设置其继承Obj类型
+
+    //init obj_klass
+    Type* obj_type = new Type();
+    obj_type->setSign(object_klass);
+    object_klass->setSuper(nullptr);
+
+
+    type_klass->set_klass_dict(new Dict());
+    object_klass->set_klass_dict(new Dict());
+
+
+    type_klass->setName(new String("type"));//新建一个类型
+    object_klass->setName(new String("object"));//新建一个类型
+
+
+
 }
 
 void Universe::destroy() {
